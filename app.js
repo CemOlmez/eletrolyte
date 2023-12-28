c8f06f324032ccd71e5e8fa62919c6bdc70f3ee3
CemOlmez <c.olmez123@gmail.com>
2023-12-28 19:48:51 +0300

    new repo

---
+++ app.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".toggle-links");
+    const icon = document.querySelector(".toggle-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
